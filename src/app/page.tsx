472aac1e864fc73d7c5327b663718375147be2ad
hin6150 <hin6150@gmail.com>
2024-11-25 22:59:07 +0900

    feat: 랜딩 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-import { useRef, useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import Image from 'next/image'
 import SnowfallCanvas from '@/components/SnowfallCanvas'
+import { Button } from '@/components/ui/button'
 import {
-  ClockIcon,
   AcademicCapIcon,
-  UserGroupIcon,
-  CheckCircleIcon, // BadgeCheckIcon 대체
+  ArchiveBoxIcon,
+  CheckCircleIcon,
+  ClockIcon, // BadgeCheckIcon 대체
   PencilIcon,
   PresentationChartLineIcon,
   TrophyIcon,
-  ArchiveBoxIcon,
+  UserGroupIcon,
 } from '@heroicons/react/24/solid' // Heroicons 추가
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
 
 interface EventItem {
   time: string
@@ -26,7 +26,6 @@ interface EventItem {
 const HomePage = () => {
   const containerRef = useRef<HTMLDivElement>(null)
   const timetableRef = useRef<HTMLDivElement>(null)
-  const router = useRouter()
   const [timeLeft, setTimeLeft] = useState<string>('')
   const [showTimetable, setShowTimetable] = useState(false)
   const [visibleItems, setVisibleItems] = useState<number[]>([])
@@ -53,7 +52,7 @@ const HomePage = () => {
     {
       time: '11:30',
       title: '점심 식사',
-      description: [],
+      description: ['중식 제공'],
       icon: <CheckCircleIcon className="mr-2 h-5 w-5 text-green-700" />, // BadgeCheckIcon 대체
     },
     {
@@ -73,7 +72,7 @@ const HomePage = () => {
     {
       time: '16:30',
       title: '우수자 시상',
-      description: [],
+      description: ['참가자들의 투표 & 게임 진행'],
       icon: <TrophyIcon className="mr-2 h-5 w-5 text-green-700" />,
     },
     {
@@ -93,7 +92,7 @@ const HomePage = () => {
       const difference = targetDate.getTime() - now.getTime()
 
       if (difference <= 0) {
-        setTimeLeft('00:00:00')
+        setTimeLeft('00시 00분 00초')
         clearInterval(intervalId)
         return
       }
@@ -103,15 +102,24 @@ const HomePage = () => {
       const minutes = Math.floor((difference / (1000 * 60)) % 60)
       const seconds = Math.floor((difference / 1000) % 60)
 
-      const totalHours = days * 24 + hours
+      // const totalHours = days * 24 + hours
 
-      const formattedTime = [
-        totalHours.toString().padStart(2, '0'),
-        minutes.toString().padStart(2, '0'),
-        seconds.toString().padStart(2, '0'),
-      ].join(':')
+      // const formattedTime = [
+      //   totalHours.toString().padStart(2, '0'),
+      //   minutes.toString().padStart(2, '0'),
+      //   seconds.toString().padStart(2, '0'),
+      // ].join(':')
 
-      setTimeLeft(formattedTime)
+      setTimeLeft(
+        days.toString().padStart(2, '0') +
+          '일 ' +
+          hours.toString().padStart(2, '0') +
+          '시 ' +
+          minutes.toString().padStart(2, '0') +
+          '분 ' +
+          seconds.toString().padStart(2, '0') +
+          '초 ',
+      )
     }
 
     updateCountdown()
@@ -159,8 +167,8 @@ const HomePage = () => {
       console.log(
         `scrollTop: ${scrollTop}, scrollHeight: ${scrollHeight}, clientHeight: ${clientHeight}, scrollFraction: ${scrollFraction}`,
       )
-
-      if (scrollFraction > 0.3) {
+      console.log(scrollFraction)
+      if (scrollFraction > 0.1) {
         setShowTimetable(true)
       } else {
         setShowTimetable(false)
@@ -182,47 +190,62 @@ const HomePage = () => {
   const handleArrowClick = () => {
     console.log('Arrow clicked, scrolling to timetable')
     timetableRef.current?.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    // setShowTimetable(true)
   }
 
   return (
     <div className="relative flex h-screen w-full items-center justify-center bg-slate-100">
       <div
         ref={containerRef}
-        className="no-scrollbar relative z-10 flex h-full w-full max-w-[600px] flex-col items-center justify-start gap-8 overflow-y-scroll rounded bg-white p-8 shadow-lg"
+        className="no-scrollbar g-12 relative z-10 flex h-full w-full max-w-[600px] flex-col items-center justify-start gap-8 overflow-y-scroll rounded bg-white p-8 shadow-lg"
       >
         <SnowfallCanvas containerRef={containerRef} />
-        <div className="relative z-20 mt-20 flex flex-col items-center gap-10">
-          <Image
-            src="/image/gdg_icon.svg"
-            alt="gdg_icon"
-            width={208}
-            height={104}
-          />
-          <div className="flex flex-col items-center gap-2">
-            <h1 className="text-3xl font-bold">11/30 IT 기획캠프</h1>
-            <h1 className="mt-2 text-3xl font-bold">{timeLeft}</h1>
+        <div className="flex min-h-screen flex-col justify-between py-32">
+          <div className="relative z-20 mt-20 flex flex-col items-center gap-14">
+            <Image
+              src="/image/gdg_icon.svg"
+              alt="gdg_icon"
+              width={208}
+              height={104}
+            />
+            <div className="flex flex-col items-center gap-2 bg-white p-4">
+              <h1 className="text-2xl font-bold">
+                제목은 IT 기획캠프로 하겠습니다.
+              </h1>
+              <p className="pb-8 text-xl">그런데, 이제 PM을 곁들인</p>
+
+              <h1 className="text-2xl font-bold">GDG X IT 기획캠프</h1>
+              <p className="mt-2 text-xl font-bold">선착순 모집중!</p>
+              <p className="mt-2 text-xl">{timeLeft}후 시작</p>
+            </div>
+            <div
+              className={`relative z-20 mt-10 w-[280px] flex-col gap-3 ${timeLeft == '00:00:00' ? 'flex' : 'hidden'}`}
+            >
+              <Link href="/admin/product">
+                <Button className="w-full bg-blue-500 py-2 text-white hover:bg-blue-400">
+                  관리자 로그인
+                </Button>
+              </Link>
+              <Link href="/product">
+                <Button className="w-full bg-blue-500 py-2 text-white hover:bg-blue-400">
+                  사용자 로그인
+                </Button>
+              </Link>
+            </div>
           </div>
-          <div className="relative z-20 mt-10 flex w-[280px] flex-col gap-3">
-            <Button className="w-full bg-blue-500 py-2 text-white hover:bg-blue-400">
-              관리자 로그인
-            </Button>
-            <Button className="w-full bg-blue-500 py-2 text-white hover:bg-blue-400">
-              사용자 로그인
-            </Button>
+          <div
+            onClick={handleArrowClick}
+            className="z-20 -translate-x-1/2 transform animate-float cursor-pointer"
+            aria-label="타임테이블로 스크롤"
+          >
+            <Image
+              src="/image/arrow.png"
+              alt="화살표 이미지"
+              width={150}
+              height={150}
+              className="h-auto w-full"
+            />
           </div>
-        </div>
-        <div
-          onClick={handleArrowClick}
-          className="animate-float z-20 -translate-x-1/2 transform cursor-pointer"
-          aria-label="타임테이블로 스크롤"
-        >
-          <Image
-            src="/image/arrow.png"
-            alt="화살표 이미지"
-            width={150}
-            height={150}
-            className="h-auto w-full"
-          />
         </div>
         <div
           ref={timetableRef} // 타임테이블 섹션 참조 추가
@@ -264,6 +287,13 @@ const HomePage = () => {
               ))}
             </div>
           </div>
+        </div>
+        <div>
+          <Link target="_blank" href="https://forms.gle/D8VQnaCvFCgcb6Tj7">
+            <Button className="w-full bg-blue-500 py-2 text-white hover:bg-blue-400">
+              지금 바로 신청하기
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
